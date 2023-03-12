d0d7ab01628063bdb1bd55ec1f272fba66287110
bsullender <35014921+b-sullender@users.noreply.github.com>
2023-03-12 18:24:18 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # base64
-Base64 implementation
+Base64 implementation by Simon Josefsson, modified by Brian Sullender.
+
+You do not need to compile anything to use base64, just include **`base64.cpp`** and **`base64.h`** in your application.
+
+See **`example.cpp`** for a detailed example.
+
+## Base64 Resources
+- [Base64 specification](https://www.rfc-editor.org/rfc/rfc4648)
+- [Implementation by Simon Josefsson](https://josefsson.org/base-encoding/)
---
+++ example.cpp
@@ -0,0 +1,64 @@
+
+//
+// example.cpp
+// 
+// Author:
+//     Brian Sullender
+//     SULLE WAREHOUSE LLC
+// 
+// Description:
+//     The example program for using base64
+//
+
+#include <iostream>
+#include "base64.h"
+
+int main()
+{
+	char* out;
+	char* in = "Some text 1234";
+	size_t inlen = strlen(in);
+
+	printf("Data: %s\n", in);
+
+	// Encode the data
+	size_t outlen = base64_encode_alloc(in, inlen, &out);
+	if (outlen == 0) {
+		printf("%s", "Encode error: input too long");
+		return -1;
+	}
+	if (out == NULL) {
+		printf("%s", "Encode error: memory allocation error");
+		return -1;
+	}
+
+	printf("Encoded: %s\n", out);
+
+	// Our input will be the previous output
+
+	in = out;
+	inlen = outlen;
+
+	// Decode the data
+	bool ok = base64_decode_alloc(in, inlen, &out, &outlen);
+	if (!ok) {
+		printf("%s", "Decode error: input was not valid base64");
+		return -1;
+	}
+	if (out == NULL) {
+		printf("%s", "Decode error: memory allocation error");
+		return -1;
+	}
+
+	// Set terminating NULL character
+	out[outlen] = '\0';
+
+	printf("Decoded: %s\n", out);
+
+	// Free resources
+	free(out);
+	free(in);
+
+	// Exit
+	return 0;
+}
